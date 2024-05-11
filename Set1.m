abf5cd34c762bc86653f01a51f2effe1ce7a5550
mohammad-amin-safaei <112809146+mohammad-amin-safaei@users.noreply.github.com>
2024-05-11 13:00:59 +0330

    Add files via upload

---
+++ Set1.m
@@ -0,0 +1,17 @@
+%First question
+A=randn(6,8)
+%second question : zeros() function generates a matrix with a certain dimension containing only zeros as elements 
+%  but flase() generates a matrix which contains boolean values.(logician
+%  ones)
+%third question:
+B=rand(4,5)
+t=B'
+
+%Fourth question
+m=[9 2 6;23 3 12;6 6 16]
+n=[5 22 5;26 59 16;24 0 16]
+
+ans1=m*n 
+ans2=m.*n
+r=mod(m,n)
+
